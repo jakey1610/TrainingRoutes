2dd9eeb2c2f67781d3cb496e13c7172620e4c236
jakey1610 <mortimer907@gmail.com>
2017-12-10 03:07:37 +0000

    Added stylesheet and implemented change of table functionality

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <!DOCTYPE html>
 <html>
-	<script src="jquery-3.2.1.min.js"></script>
+	<head>
+		<link href = "stylesheet.css" rel = "stylesheet" type = "text/css" />
+		<script src="jquery-3.2.1.min.js"></script>
+	</head>
+	
 	<h1></h1>
+	<h2>Table Number: <input type = "text" name = "tn" id="tn"/>
+	<input type = "submit" value = "Confirm" onClick = "searchTN()"/></h2>
 	<script>
+		tableNum = 1;
     $(function(){
       function loadNum()
       {  
-        $('h1').load('query.php');
+        $('h1').load('query.php?tableNum=' + tableNum);
         setTimeout(loadNum, 3000);
       }
       loadNum();
     });
+
+    function searchTN() {
+    	tableNum = document.getElementById("tn").value;
+    	$.get("query.php?tableNum=" + tableNum).done(function(data){
+    		$("h1").html(data);
+    	});
+    }
+
+
  	</script>
 </html>
--- query.php
+++ query.php
@@ -3,26 +3,27 @@
 	if ($mysqli->connect_error) {
 		die('Connect Error: ' . $mysqli->connect_error);
 	}
-
+	$tableNum = $_GET["tableNum"];
 	$qrymaxtn = "SELECT MAX(tableNum) AS mtn FROM ordersDB.order;";
 	$resultmtn = mysqli_query($mysqli, $qrymaxtn);
 	while($row3 = mysqli_fetch_array($resultmtn)) {
-		$string1 = $row3["mtn"];
+		$int1 = settype($row3["mtn"], "integer");
 	}
-	//for($tableNum = 1; $tableNum <= 2; $tableNum){
-		echo "<p>Orders for table ".$tableNum.": </p>";
-		$qry = "SELECT nameMeal, price FROM ordersDB.order WHERE tableNum=$tableNum;";
-		$result = mysqli_query($mysqli, $qry);
-		while($row = mysqli_fetch_array($result)) { 
-			echo("<p>".$row["nameMeal"]." -- ".$row["price"]."</p>"); 
-		}
-		$qrytotalprice = "SELECT SUM(price) AS total FROM ordersDB.order WHERE tableNum = $tableNum;";
-		$result2 = mysqli_query($mysqli, $qrytotalprice);
+	echo "<p id = 'title'><u>Orders for table ".$tableNum.": </u></p>";
+	$qry = "SELECT nameMeal, price FROM ordersDB.order WHERE tableNum=$tableNum;";
+	$result = mysqli_query($mysqli, $qry);
+	echo "<ul>";
+	while($row = mysqli_fetch_array($result)) { 
+		echo("<li><p>".$row["nameMeal"]." -- ".$row["price"]."</p></li>"); 
+	}
+	echo "</ul>";
+	$qrytotalprice = "SELECT SUM(price) AS total FROM ordersDB.order WHERE tableNum = $tableNum;";
+	$result2 = mysqli_query($mysqli, $qrytotalprice);
 
-		while($row2 = mysqli_fetch_array($result2)) {
-			$string = number_format($row2["total"],2);
-		}
-		echo "<p>Total: ".$string."</p>";
-		mysqli_close($mysqli);
-	.//}
+	while($row2 = mysqli_fetch_array($result2)) {
+		$string = number_format($row2["total"],2);
+	}
+	echo "<p>Total: ".$string."</p>";
+	mysqli_close($mysqli);
+
 ?>
