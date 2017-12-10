24831436b59d3609d4e1c3b07c677381ae428782
jakey1610 <mortimer907@gmail.com>
2017-12-10 02:21:11 +0000

    Added total

--- query.php
+++ query.php
@@ -3,13 +3,26 @@
 	if ($mysqli->connect_error) {
 		die('Connect Error: ' . $mysqli->connect_error);
 	}
-	$tableNum = 1;
-	echo "<p>Orders for table ".$tableNum.": </p>";
-	$qry = "SELECT nameMeal, price FROM ordersDB.order WHERE tableNum=$tableNum;";
-	$result = mysqli_query($mysqli, $qry);
-	//echo($result);
-	while($row = mysqli_fetch_array($result)) echo("<p>".$row["nameMeal"]." ".$row["price"]."</p>");
-	//$qrytotalprice = $mysqli->prepare('SELECT SUM(price) FROM order WHERE table = ?');
-	//echo $qrytotalprice->execute(array($tableNum));
-	mysqli_close($mysqli);
+
+	$qrymaxtn = "SELECT MAX(tableNum) AS mtn FROM ordersDB.order;";
+	$resultmtn = mysqli_query($mysqli, $qrymaxtn);
+	while($row3 = mysqli_fetch_array($resultmtn)) {
+		$string1 = $row3["mtn"];
+	}
+	//for($tableNum = 1; $tableNum <= 2; $tableNum){
+		echo "<p>Orders for table ".$tableNum.": </p>";
+		$qry = "SELECT nameMeal, price FROM ordersDB.order WHERE tableNum=$tableNum;";
+		$result = mysqli_query($mysqli, $qry);
+		while($row = mysqli_fetch_array($result)) { 
+			echo("<p>".$row["nameMeal"]." -- ".$row["price"]."</p>"); 
+		}
+		$qrytotalprice = "SELECT SUM(price) AS total FROM ordersDB.order WHERE tableNum = $tableNum;";
+		$result2 = mysqli_query($mysqli, $qrytotalprice);
+
+		while($row2 = mysqli_fetch_array($result2)) {
+			$string = number_format($row2["total"],2);
+		}
+		echo "<p>Total: ".$string."</p>";
+		mysqli_close($mysqli);
+	.//}
 ?>
